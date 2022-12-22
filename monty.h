b6e80df32c6db5d64fb24d17536afd83ebba9810
nadaMAoui <maaouinada747@gmail.com>
2022-12-22 10:27:39 +0100

    die

--- monty.h
+++ monty.h
@@ -1,11 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
-#ifndef MONTY
-#define MONTY
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer

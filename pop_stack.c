3324f09daad0232a18a75aa1b74feeecea4620c9
nadaMAoui <maaouinada747@gmail.com>
2022-12-22 06:20:51 +0100

    emoves the top element of the stack

---
+++ pop_stack.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+#include <string.h>
+/**
+ * delete_stack_head - delete the front of the queue
+ */
+
+int deQueue(stack_t **head)
+{
+	stack_t *tmp;
+
+	if (*head == NULL)
+		return (-1);
+	tmp = (*head);
+	if (tmp->next == NULL)
+	{
+		free(tmp);
+		(*head) = NULL;
+		return (1);
+	}
+	(*head) = (*head)->next;
+	(*head)->prev = NULL;
+	free(tmp);
+	return (1);
+}
+/**
+ * stk_pop - removes the top element of the stack
+ */
+
+void stack_pop(stack_t **stack, unsigned int ln)
+{
+	int result;
+
+	result = deQueue(stack);
+	if (result != 1)
+	{
+		printf("L%d: can't pop an empty stack\n", ln);
+		value[2] = 1;
+		return;
+	}
+}
